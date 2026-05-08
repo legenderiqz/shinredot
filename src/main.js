d56660ef3f076476c91234f7d18785a91cbea0b6
legenderiqz <egemenlegender16@gmail.com>
2026-05-08 21:11:19 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,16 +4,14 @@
 // alt => üst X
 
 import { render, initRendering } from './render.js';
-import { initUI } from './ui.js';
-import { update } from './update.js';
-import { canvas } from './data/dom.js';
+import { initUI, initMenuUI, initSettingsUI, statsUI, resetUI } from './ui.js';
+import { update, queueSave } from './update.js';
+import { getCanvas, initDOM } from './data/dom.js';
 import { loadLevel } from './render.js';
 import { gs } from './data/state.js';
 
-//ctx
-const ctx = canvas.getContext('2d');
-
 let lastTime = 0;
+let ctx;
 
 function loop(timestamp) {
   if (!lastTime) lastTime = timestamp;
@@ -21,15 +19,47 @@ function loop(timestamp) {
   lastTime = timestamp;
 
   update(deltaTime);
-  render(ctx);
+  render(gs.ctx);
   requestAnimationFrame(loop);
 }
 
-async function init() {
+
+function listenMenus() {
+  if (gs.statsRequest) {
+    statsUI();
+    gs.statsRequest = false;
+  }
+  if (gs.resetRequest) {
+    resetUI();
+    gs.resetRequest = false;
+  }
+  requestAnimationFrame(listenMenus)
+}
+
+
+async function initGame() {
+  initDOM();
+  const canvas = getCanvas();
+
+  if (!canvas) {
+    console.error('Canvas bulunamadı');
+    return;
+  }
+
+  ctx = canvas.getContext('2d');
+
+  if (!ctx) {
+    console.error('CTX alınamadı');
+    return;
+  }
+
+  gs.ctx = ctx;
+
   initUI();
   initRendering(ctx);
+
   await loadLevel(gs.currentLevelId);
-  setRealHeight();
+  queueSave();
   requestAnimationFrame(loop);
 }
 
@@ -40,9 +70,36 @@ function setRealHeight() {
   });
 }
 
-setRealHeight();
+function initPage() {
+  gs.scene = 'menu';
+  initMenuUI();
+  checkPlaying();
+  loadGame();
+  initSettingsUI();
+  listenMenus();
+}
 
-init();
+function checkPlaying() {
+  if (gs.scene === 'game') {
+    initGame();
+  } else if (gs.scene === 'menu') {
+    requestAnimationFrame(checkPlaying);
+  }
+}
+
+export function loadGame() {
+  const data = JSON.parse(localStorage.getItem('shinredot_save'));
+  if (!data) return;
+  
+  gs.totalCoins = data.totalCoins ?? 0;
+  gs.currentLevelId = data.currentLevelId ?? 1;
+  gs.currentRoomId = data.currentRoomId ?? 1;
+  gs.totalJumps = data.totalJumps ?? 0;
+  gs.totalShadows = data.totalShadows ?? 0;
+}
+
+setRealHeight();
+initPage();
 
 window.addEventListener('resize', setRealHeight);
 window.addEventListener('orientationchange', setRealHeight);
